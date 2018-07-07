39e853588dd9fc6e86dc652befa6b00372e3820d
bobpepers <bob.pepers@gmail.com>
2018-07-07 12:45:59 +0200

    cleanup html, add asset

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use('/assets', [
     express.static(__dirname + '/node_modules/bootstrap/dist/js/'), //?
     express.static(__dirname + '/node_modules/waypoints/lib/'),
     express.static(__dirname + '/node_modules/particles.js/'),
+    express.static(__dirname + '/public/js/'),
 ]);
 
 app.get("/", function(request, response){ //root dir
