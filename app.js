0322ff851cfa136a27faac5478422f4126abbdc0
bobpepers <bob.pepers@gmail.com>
2018-07-08 23:51:54 +0200

    start using latest bootstrap

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var MongoClient = require('mongodb').MongoClient
 var url = "mongodb://localhost:27017/";
 var db = require('./db')
 var fs = require("fs");
-var port = 3000;
+
 var express = require("express");
 var bodyParser  = require("body-parser");
 var webhook = require('express-github-webhook');
