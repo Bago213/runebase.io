9fe02d150838e126a727647bd68bd0a60709b86c
bobpepers <bob.pepers@gmail.com>
2018-07-08 20:37:27 +0200

    switch to jade

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var MongoClient = require('mongodb').MongoClient
 var url = "mongodb://localhost:27017/";
 var db = require('./db')
+var fs = require("fs");
+var port = 3000;
+var express = require("express");
+var bodyParser  = require("body-parser");
+var webhook = require('express-github-webhook');
+var webhookHandler = webhook({ path: '/webhook', secret: '123456' });
+var app = express();
+
+app.engine('jade', require('jade').__express)
+app.set('view engine', 'jade')
+app.use(bodyParser.json());
+app.use(webhookHandler);
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
@@ -10,19 +22,6 @@ MongoClient.connect(url, function(err, db) {
     db.close();
   });
 }); 
-
-var fs = require("fs");
-var port = 3000;
-var express = require("express");
-var bodyParser  = require("body-parser");
-var webhook = require('express-github-webhook');
-var webhookHandler = webhook({ path: '/webhook', secret: '123456' });
-var app = express();
-app.engine('jade', require('jade').__express)
-app.set('view engine', 'jade')
-
-app.use(bodyParser.json());
-app.use(webhookHandler);
 
 webhookHandler.on('push', function (repo, data) {
   MongoClient.connect(url, function(err, db) {
@@ -36,7 +35,8 @@ webhookHandler.on('push', function (repo, data) {
   });
 });
 
-app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
+// app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
+
 app.use('/assets', [
     express.static(__dirname + '/node_modules/jquery/dist/'),
     express.static(__dirname + '/node_modules/jquery.easing/'),
@@ -49,13 +49,13 @@ app.use('/assets', [
     express.static(__dirname + '/public/images/'),
     express.static(__dirname + '/public/css/'),
 ]);
-app.use('/commits', require('./controllers/commits'))
 
-app.get("/", function(request, response){ //root dir
-    response.send(express.static(__dirname + "/public"));
-});
+app.use('/commits', require('./controllers/commits'));
 
-//app.listen(port, () => console.log('app listening on port ' + port))
+app.get('/', function (req, res) {
+  res.render('index', {option: 'value'});
+})
+
 db.connect(url, function(err) {
   if (err) {
     console.log('Unable to connect to Mongo.')
