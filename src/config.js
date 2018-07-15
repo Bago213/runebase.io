dde4208c32f0255590d4e77c7e7e7c2e946f7ac7
bobpepers <bobpepers@gmail.com>
2018-07-15 05:39:42 +0200

    fix gitignore

---
+++ src/config.js
@@ -0,0 +1,9 @@
+var config = {};
+config.host = 'http://www.runebase.io/'
+config.db_host = 'mongodb://localhost:27017/';
+config.github_client_id = "9bcbc9a960328583498f";
+config.github_client_secret = "123456";
+config.session_secret = "Custom Session Secret";
+config.webhook_secret = "123456";
+
+module.exports = config;
