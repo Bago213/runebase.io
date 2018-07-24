e9aeb374c0215ddee0abcd18403a9d481e9a5097
bobpepers <bobpepers@gmail.com>
2018-07-24 12:58:40 +0200

    fixed dataset announcement section

--- public/javascripts/angular.custom.index.js
+++ public/javascripts/angular.custom.index.js
@@ -10,6 +10,17 @@ app.controller('MainCtrl', function($scope, NgTableParams, $http) {
 
     $scope.getData("https://api.github.com/orgs/runebase/issues").then(function(data) {
         console.log(data);
+        data = [
+        {
+            time: "bitcoin",
+            message: "Some Sort of Announcement"
+        },
+        {
+            time: "runebase",
+            message: "Some Sort of Announcement"
+        }
+        ];
+        console.log(data);
         $scope.tableParams = new NgTableParams({count: data.length}, {counts: [], dataset: data});        
     });
 
