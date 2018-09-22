dfa2320d05d85e95184c47a65afefadd052e126a
bobpepers <bobpepers@gmail.com>
2018-09-22 08:22:20 +0200

    angular API calls and loading animation for angular API request + placeholder for Price

--- public/javascripts/angular.custom.index.js
+++ public/javascripts/angular.custom.index.js
@@ -1,28 +1,34 @@
-var app = angular.module('tables', ["ngTable"]);
-app.controller('MainCtrl', function($scope, NgTableParams, $http) {
-    $scope.getData = function($query){
-        $scope.result = $http.get($query).then(
-            function($response){
-                return $response.data;
-            }, function($response){ return [];});
-            return $scope.result;
-    }
-
-    $scope.getData("https://api.github.com/orgs/runebase/issues").then(function(data) {
-        console.log(data);
-        data = [
-        {
-            time: "bitcoin",
-            message: "Some Sort of Announcement"
-        },
-        {
-            time: "runebase",
-            message: "Some Sort of Announcement"
-        }
-        ];
-        console.log(data);
-        $scope.tableParams = new NgTableParams({count: data.length}, {counts: [], dataset: data});        
-    });
-
-});
+var app = angular.module('myApp', []);
+app.controller('myController',['$q', '$scope','$http',function($q,$scope,$http){
+        $scope.$emit('LOAD');
+        $data_1 = $http.get('https://explorer.runebase.io/runebase-insight-api/supply', {cache: false});
+        $data_2 = $http.get('https://explorer.runebase.io/runebase-insight-api/supply', {'cache': false}); //Place holder for Price Request or other API get
+        $q.all([$data_1, $data_2])
+        .then(function(data){
+            console.log(data[0]);
+            console.log(data[1]); // Placeholder console log price request
+            var blockNumber=data[0].data/100;
+            var supply=data[0].data;
+            data = [
+            {
+                title: "Minted Blocks",
+                message: blockNumber
+            },
+            {
+                title: "Circulating Supply",
+                message: supply
+            },
+            {
+                title: "Current Price",
+                message: "0"
+            }
+            ];
+            console.log(data);
+            $scope.coinData = data;   
+            $scope.$emit('UNLOAD');
+        })
+    }]).controller('MainCtrl',['$scope',function($scope){
+            $scope.$on('LOAD',function(){$scope.loading=true});
+            $scope.$on('UNLOAD',function(){$scope.loading=false});
+        }])
   
